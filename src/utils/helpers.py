13af5a38a17b5718ebd8065e2b7dc47837c2617d
0001024bytes <93302536+0001024bytes@users.noreply.github.com>
2024-10-24 14:40:37 -0300

    refactor: add `redis`

---
+++ src/utils/helpers.py
@@ -0,0 +1,9 @@
+def calculate_percentage(x: float, y: float) -> float:
+    """
+    Calculate the percentage of a value.
+
+    Args:
+        x (float): Value to calculate the percentage of.
+        y (float): Percentage value.
+    """
+    return (x * y) / 100.0
