944ec3ac12857e6c4d820e6f330db51d39c889a9
0001024bytes <93302536+0001024bytes@users.noreply.github.com>
2024-10-24 14:40:01 -0300

    refactor: add `coinos` webhook

--- src/api/__init__.py
+++ src/api/__init__.py
@@ -1,7 +1,8 @@
 from fastapi.middleware.cors import CORSMiddleware
-from fastapi import FastAPI, HTTPException, Query
 from src.services.bitfinex import bitfinex
 from src.services.coinos import coinos
+from src.services.redis import redis
+from src.api.schemas import CoinosWebhookSchema
 from src.configs import (
     API_ALLOW_ORIGINS,
     API_HOST,
@@ -10,6 +11,10 @@ from src.configs import (
     COINOS_WEBHOOK_KEY,
     COINOS_WEBHOOK_URL,
 )
+from src.lib.lnurl import LightningAddress
+from fastapi import FastAPI, HTTPException, Query
+from uuid import uuid4
+
 import uvicorn
 import re
 
@@ -34,36 +39,70 @@ api.add_middleware(
 async def create_address(
     id: str,
     amount: int = Query(0, ge=0, description="Amount to be donated in fiat."),
+    lightning_address: str = Query(""),
     payment_type: str = Query("lightning", description="Payment type. E.g., lightning or liquid."),
     message: str = Query("", description="Optional message.")
 ):
-    # Validate payment type
     valid_payment_types = ["lightning", "liquid"]
     if payment_type not in valid_payment_types:
         raise HTTPException(status_code=400, detail="Payment type is invalid.")
 
-    # Get price and calculate the amount in satoshis
     price_data = bitfinex.get_price(ticket="btcusd")
     price = float(price_data["SELL"])
     amount_btc = amount / price
-    amount_sat = round(amount_btc * 1e8)
+    amount_sat = round(amount_btc * pow(10, 8))
 
-    # Create the invoice in Coinos
+    lnurlp_info = LightningAddress.get_lnurlp_info(address)
+    if len(message) > lnurlp_info.get("commentAllowed", 0):
+        raise ValueError("The comment size is larger than allowed.")
+    
+    if lnurlp_info.get("minSendable", 0) > amount_sat * 1000:
+        raise ValueError("Minimum value is greater than amount.")
+
+    if amount_sat * 1000 > lnurlp_info.get("maxSendable", 0):
+        raise ValueError("The amount value is greater than the maximum value.")
+
+    txid = uuid4()
+    url = f"{COINOS_WEBHOOK_URL}/{txid}/{id}/{payment_type}?message={message}&lightning_address={lightning_address}"
     invoice = coinos.create_invoice(
         amount_sat,
         payment_type.lower(),
-        f"{COINOS_WEBHOOK_URL}?id={id}&message={message}",
+        url,
         COINOS_WEBHOOK_KEY
     )
-
-    # Modify the invoice if the payment type is liquid
     if payment_type == "liquid":
         match = re.search(r'liquidnetwork:([^?]+)', invoice["text"])
         if match:
             address = match.group(1)
             invoice["text"] = f"liquidnetwork:{address}?amount={amount_btc:.8f}&assetid=6f0279e9ed041c3d710a9f57d0c02928416460c4b722ae3457a11eec381c526d"
 
-    return {"address": invoice["text"]}
+    return {"txid": txid, "address": invoice["text"]}
+
+@api.get("/api/v1/payment/{txid}/paid")
+def get_payment_paid(txid: str):
+    tx = redis.redis_get(f"tx.{txid}")
+    return {"paid": tx.get("paid", False)}
+
+@api.post("/api/v1/coinos/webhook/{txid}/{id}/{payment_type}")
+def coinos_webhook_payment(
+        txid: str, 
+        id: str, 
+        payment_type: str, 
+        data: CoinosWebhookSchema,
+        lightning_address: str = Query("", description="Optional lightning address."),
+        message: str = Query("", description="Optional message.")
+    ):
+    data = dict(data.dict())
+    if data["secret"] != COINOS_WEBHOOK_KEY:
+        raise HTTPException(400)
+
+    data["id"] = id
+    data["txid"] = txid
+    data["payment_type"] = payment_type
+    data["message"] = message
+    data["paid"] = data.get("confirmed", False)
+    data["lightning_address"] = lightning_address
+    redis.redis_set(f"tx.{txid}", data)
 
 def start():
     """
--- src/configs.py
+++ src/configs.py
@@ -9,8 +9,25 @@ API_ALLOW_ORIGINS = getenv("API_ALLOW_ORIGINS", "*").split(",")
 # Production configuration
 PRODUCTION = getenv("PRODUCTION", "false") == "true"
 
+class RedisConfig:
+
+    def __getattr__(self, name: str):
+        value = getenv(name)
+        if value:
+            if name == "REDIS_PREFIX":
+                value = f"L#{value}"
+            if name == "REDIS_PORT":
+                value = int(value)
+            return value
+        elif name == "REDIS_PREFIX":
+            return "T#"
+        elif name == "REDIS_HOST":
+            return "127.0.0.1"
+        elif name == "REDIS_PORT":
+            return 6379
+
 # Coinos configuration
 COINOS_USERNAME = getenv("COINOS_USERNAME")
 COINOS_PASSWORD = getenv("COINOS_PASSWORD")
-COINOS_WEBHOOK_URL = getenv("WEBHOOK_URL")
-COINOS_WEBHOOK_KEY = getenv("WEBHOOK_KEY", token_hex(32))
+COINOS_WEBHOOK_URL = getenv("COINOS_WEBHOOK_URL", f"http://localhost:{API_PORT}/api/v1/coinos/webhook")
+COINOS_WEBHOOK_KEY = getenv("COINOS_WEBHOOK_KEY", token_hex(32))
