372a62524621d0fb40b591f3e5ef545a24855d0a
0001024bytes <93302536+0001024bytes@users.noreply.github.com>
2024-10-27 16:17:37 -0300

    refactor: add `depix`

---
+++ src/lib/depix.py
@@ -0,0 +1,23 @@
+import requests
+
+class Depix:
+
+    def __init__(
+            self, 
+            url: str = "http://localhost:8080",
+            key: str = None
+        ) -> None:
+        self.url = url
+        self.key = key
+
+    def call(self, method: str, path: str, data=None) -> dict:
+        response = requests.request(
+            method=method, 
+            url=self.url + path,
+            json=data,
+            headers={ "X-API-KEY": self.key }
+        )
+        return response.json()
+
+    def create_qrcode(self, amount: float, address: str) -> dict:
+        return self.call("POST", "/api/v1/qrcode", data={"amount": amount, "address": address})
---
+++ src/services/depix.py
@@ -0,0 +1,4 @@
+from src.lib.depix import Depix
+from src.configs import DEPIX_KEY, DEPIX_URL
+
+depix = Depix(url=DEPIX_URL, key=DEPIX_KEY)
